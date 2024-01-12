ad9987f7dd27cca0e67440bd057628f666cbba63
usuario <usuario@Marcos.mshome.net>
2024-01-12 12:29:12 +0100

    Commit 1: comprobación de práctica

---
+++ EjercicioInventario/src/main/java/com/proyecto/ejercicioinventario/EjercicioInventario.java
@@ -0,0 +1,20 @@
+/**
+ * Inventario de Tienda: Escribe un programa para gestionar el inventario de una 
+ * tienda utilizando ArrayList. Debe permitir agregar productos con detalles como 
+ * nombre, precio y cantidad en stock, así como realizar operaciones como actualizar 
+ * inventario, buscar productos y mostrar el inventario actualizado.
+ */
+
+package com.proyecto.ejercicioinventario;
+
+/**
+ *
+ * @author Marcos
+ * @version 1.0
+ */
+public class EjercicioInventario {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
